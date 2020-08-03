26ee2354ed3588c4a95c1bc4b93c49907461eced
Syed <imran.syed@capgemini.com>
2020-08-03 13:06:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'pycurry';
+
+  num1: number;
+  num2: number;
+  result: number;
+  add() {
+      this.result = this.num1 + this.num2;
+  }
+  substract() {
+      this.result = this.num1 - this.num2;
+  }
+  multiply() {
+      this.result = this.num1 * this.num2;
+  }
+  divide() {
+      this.result = this.num1 % this.num2;
+  }
 }
